0fbe9624d903c148b6b125837c0daea2939bbf92
Amarpreet Singh <amarpreet2209@gmail.com>
2017-08-15 19:01:13 +0530

    Passport added to Sign Up

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ var bodyParser = require('body-parser');
 var expressHbs = require('express-handlebars');
 var mongoose = require('mongoose')
 var session = require('express-session');
+var passport = require('passport');
+var flash = require('connect-flash');
 
 var index = require('./routes/index');
 
@@ -14,7 +16,7 @@ var app = express();
 
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost:27017/shopping', { useMongoClient: true });
-
+require('./config/passport');
 
 // view engine setup
 app.engine('.hbs', expressHbs({defaultLayout: 'layout', extname: '.hbs'}));
@@ -27,6 +29,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(session({secret: 'mysupersecret', resave: false, saveUninitialized: false}));
+app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
